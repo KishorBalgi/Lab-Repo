7ccf289d3d8498c3b006b5937b74997e910bb2f9
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-11-25 20:06:53 +0530

    Server bug fixes

--- modules/codeviewer.js
+++ modules/codeviewer.js
@@ -1,9 +1,9 @@
 module.exports = (temp, data) => {
-  let code = data.code.replace(/;/g, ";<br/>");
-  code = code.replace(/{/g, "{<br/>");
-  code = code.replace(/}/g, "<br/>}");
+  let code = data.code.replace(/;/g, ";<br>");
+  code = code.replace(/{/g, "{<br>");
+  code = code.replace(/}/g, "<br>}");
   code = code.replace(/</g, "&lt;");
-  code = code.replace(/>/g, "&gt;<br/>");
+  code = code.replace(/>/g, "&gt;<br>");
 
   let output = temp.replace(/{%TITLE%}/g, data.id.toUpperCase());
   output = output.replace(/{%TIME%}/g, Date(data.timestamp));
