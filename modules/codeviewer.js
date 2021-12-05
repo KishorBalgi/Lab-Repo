cfc05326d4f022999565eced9179b581144a31e4
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-12-05 22:43:35 +0530

    Fixed date issues

--- modules/codeviewer.js
+++ modules/codeviewer.js
@@ -3,7 +3,8 @@ module.exports = (temp, data) => {
   code = code.replace(/>/g, "&gt;");
 
   let output = temp.replace(/{%TITLE%}/g, data.id.toUpperCase());
-  output = output.replace(/{%TIME%}/g, data.timestamp.toDate());
+  output = output.replace(/{%TIME%}/g, new Date(data.timestamp.seconds*1000));
   output = output.replace(/{%CODE%}/g, code);
+
   return output;
 };
