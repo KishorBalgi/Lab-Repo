c21dd4f75b6c36581344de7faff2f3cd7b25a6d5
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-12-06 15:31:57 +0530

    Fixed date timezone

--- modules/codeviewer.js
+++ modules/codeviewer.js
@@ -1,11 +1,13 @@
 module.exports = (temp, data) => {
-  // const date=new Date(data.timestamp.seconds*1000);
-  const date=new Date(data.timestamp.seconds);
+  const date = new Date(data.timestamp.seconds * 1000);
   let code = data.code.replace(/</g, "&lt;");
   code = code.replace(/>/g, "&gt;");
 
   let output = temp.replace(/{%TITLE%}/g, data.id.toUpperCase());
-  output = output.replace(/{%TIME%}/g, date.toLocaleString("en-IN"));
+  output = output.replace(
+    /{%TIME%}/g,
+    date.toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+  );
   output = output.replace(/{%CODE%}/g, code);
   return output;
 };
