fac1baab93c3c669c6a54c64a36bf75ec3d65b08
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-12-14 21:08:45 +0530

    Added Java Code Highlighting

--- modules/codeviewer.js
+++ modules/codeviewer.js
@@ -2,6 +2,26 @@ module.exports = (temp, data) => {
   const date = new Date(data.timestamp.seconds * 1000);
   let code = data.code.replace(/</g, "&lt;");
   code = code.replace(/>/g, "&gt;");
+  // Highlightings
+  code = code.replace(
+    /\b(import|class|interface|abstract)/g,
+    (x) => `<span class="h-blue">${x}</span>`
+  );
+
+  code = code.replace(
+    /\b(private|protected|public|static|new|java|Math)/g,
+    (x) => `<span class="h-brown">${x}</span>`
+  );
+
+  code = code.replace(
+    /\b(int|float|double|void|String|char|long|short)/g,
+    (x) => `<span class="h-green">${x}</span>`
+  );
+
+  code = code.replace(
+    /\b(if|else|for|while|do|switch)/g,
+    (x) => `<span class="h-violet">${x}</span>`
+  );
 
   let output = temp.replace(/{%TITLE%}/g, data.id.toUpperCase());
   output = output.replace(
